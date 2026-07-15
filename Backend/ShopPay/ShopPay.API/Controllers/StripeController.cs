eec00e7aac741977aac1589abfafd1bfef6f467b
Shubham Kalegore <shubhamkalegore88@gmail.com>
2026-07-15 22:50:47 +0530

    feat: implimented setup for stripe webhook

--- Backend/ShopPay/ShopPay.API/Controllers/StripeController.cs
+++ Backend/ShopPay/ShopPay.API/Controllers/StripeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ShopPay.Application.DTOs.Stripe;
 using ShopPay.Application.Interfaces;
+using Stripe;
 
 namespace ShopPay.API.Controllers;
 
@@ -9,11 +10,13 @@ namespace ShopPay.API.Controllers;
 public class StripeController : ControllerBase
 {
     private readonly IStripeService _stripeService;
+    private readonly IConfiguration _configuration;
 
     public StripeController(
-        IStripeService stripeService)
+        IStripeService stripeService, IConfiguration configuration)
     {
         _stripeService = stripeService;
+        _configuration = configuration;
     }
 
     [HttpPost("create-payment-intent")]
@@ -34,5 +37,63 @@ public class StripeController : ControllerBase
             await _stripeService.VerifyPaymentAsync(paymentIntentId);
 
         return Ok(response);
+    }
+
+
+    [HttpPost("webhook")]
+    public async Task<IActionResult> Webhook()
+    {
+        var json = await new StreamReader(HttpContext.Request.Body)
+            .ReadToEndAsync();
+
+        var stripeSignature = Request.Headers["Stripe-Signature"];
+
+        var webhookSecret =
+            _configuration["Stripe:WebhookSecret"];
+
+        Event stripeEvent;
+
+        try
+        {
+            stripeEvent = EventUtility.ConstructEvent(
+                json,
+                stripeSignature,
+                webhookSecret);
+
+            Console.WriteLine($"Webhook received: {stripeEvent.Type}");
+
+            switch (stripeEvent.Type)
+            {
+                case EventTypes.PaymentIntentSucceeded:
+
+                    var paymentIntent =
+                        stripeEvent.Data.Object as PaymentIntent;
+
+                    Console.WriteLine(
+                        $"Payment Intent Id: {paymentIntent?.Id}");
+
+                    break;
+
+                case EventTypes.PaymentIntentPaymentFailed:
+
+                    var failedPaymentIntent =
+                        stripeEvent.Data.Object as PaymentIntent;
+
+                    break;
+
+                default:
+
+                    break;
+            }
+
+            return Ok();
+
+        }
+        catch (StripeException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+
+        return Ok();
     }
 }
