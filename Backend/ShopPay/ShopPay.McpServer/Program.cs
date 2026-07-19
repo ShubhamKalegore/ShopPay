489c12ada4aa5afc802fabe57dba8b2177e3d324
Shubham Kalegore <shubhamkalegore88@gmail.com>
2026-07-19 23:36:55 +0530

    mcp chnages related to project

--- Backend/ShopPay/ShopPay.McpServer/Program.cs
+++ Backend/ShopPay/ShopPay.McpServer/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using ModelContextProtocol.AspNetCore;
+using ShopPay.Application;
+using ShopPay.Infrastructure;
+using ShopPay.McpServer.Tools;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services
+    .AddMcpServer()
+    .WithHttpTransport()
+    .WithTools<ProductTools>();
+
+builder.Services.AddSingleton<ProductTools>();
+
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
+var app = builder.Build();
+
+app.MapMcp();
+
+app.Run();
---
+++ Backend/ShopPay/ShopPay.McpServer/Tools/ProductTools.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelContextProtocol.Server;
+using ShopPay.Application.DTOs;
+using ShopPay.Application.Interfaces;
+
+namespace ShopPay.McpServer.Tools;
+
+[McpServerToolType]
+public class ProductTools
+{
+    private readonly IProductService _productService;
+
+    public ProductTools(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+
+    [McpServerTool]
+    [Description("Returns all products.")]
+    public async Task<IEnumerable<ProductDto>> GetProducts()
+    {
+        return await _productService.GetAllProductsAsync();
+    }
+}  
