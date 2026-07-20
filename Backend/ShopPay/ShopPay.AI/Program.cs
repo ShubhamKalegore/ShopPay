5da999e7c7ef4d3a4643bd4db5746bc67434b4be
Shubham Kalegore <shubhamkalegore88@gmail.com>
2026-07-20 16:42:32 +0530

    created project related to mcp client

---
+++ Backend/ShopPay/ShopPay.AI/Program.cs
@@ -0,0 +1,31 @@
+﻿using ModelContextProtocol.Client;
+using ModelContextProtocol.Protocol;
+using System.Text.Json;
+
+
+var transportOptions = new HttpClientTransportOptions
+{
+    Endpoint = new Uri("http://localhost:5000")
+};
+
+var transport = new HttpClientTransport(transportOptions);
+
+var client = await McpClient.CreateAsync(transport);
+
+Console.WriteLine("Connected successfully.");
+
+var tools = await client.ListToolsAsync();
+
+foreach (var tool in tools)
+{
+    Console.WriteLine($"{tool.Name} - {tool.Description}");
+}
+
+
+var result = await client.CallToolAsync("get_products");
+
+Console.WriteLine($"IsError: {result.IsError}");
+
+var text = ((TextContentBlock)result.Content.First()).Text;
+
+Console.WriteLine(text);
